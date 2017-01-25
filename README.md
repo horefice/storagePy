07f4bf6e1583072885fe35ffad10db0b3609d437
Henrique Rodrigues Orefice <h.orefice@gmail.com>
2017-01-25 18:06:44 -0200

    init

---
+++ README.md
@@ -0,0 +1,219 @@
+[![Build Status](https://travis-ci.org/horefice/storagePy.svg?branch=master)](https://travis-ci.org/horefice/storagePy)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE.md)
+
+# storagePy
+
+With StoragePy you can instantly have a testing platform for your JSONs. That is, a solid RESTfull API + DB waits for you!
+
+## Index
+
+* [Getting Started](#getting-started)
+* [Requests](#requests)
+* [Limitations](#limitations)
+* [License](#license)
+
+## Getting Started
+
+For using the API you must first sign up and get a valid username.
+Only an email address is required for signing up.
+
+Users can sign up through:
+
+* [Official Website](http://storagepy.tk)
+* Post request
+
+One's username is calculated MD5-hashing his/her email address. <br>
+If you lose your username, you can still calculate it or sign up again (no data will be lost, I swear). <br>
+
+### Signing up at the official website
+
+Enter you email into the appropriate field at the beggining of the page. <br>
+You will receive an alert containing your assigned username as response. And that's it!
+
+### Signing up using `POST`
+
+Using a post request containing your email is enough for signing up.
+All you need to do is:
+
+* Prepare a `POST` request to `http://api.storagepy.tk/signup`
+* Add `content-Type: "application/x-www-form-urlencoded"` to headers
+* Set `email=your_email` as data
+* Don't worry about `Access-Control-Allow-Origin`
+* Wait for a 201 response containing `{ "hash" : username }`
+
+The response above means you're done and the server is ready for you.
+
+**Sample request:**
+
+```shell
+curl -X POST -H "Content-Type:application/x-www-form-urlencoded" -d 'email=johnsnow%40gmail.com' http://api.storagepy.tk/signup
+```
+
+### Is it safe?
+
+The only personal information we store is your email address. Since we hash it for signing up, anyone with access to your username will not be able to recover your email address. Besides that, we do not encourage you to put any sensitive data on our databases, it's a testing platform!
+
+## Requests
+
+All the requests must fulfill the following requirements:
+* Have `http://api.storagepy.tk/` as the base url.
+* Expect a response in `JSON` format.
+* Send data (if applicable) in `JSON` format.
+
+### Get data
+
+  Returns json data from the specified user.
+
+* **URL**
+
+  /:user_id
+
+* **Method:**
+
+  `GET`
+  
+*  **URL Params**
+ 
+   `user_id=[32-byte hex-string]`
+
+* **Data Params**
+
+  None
+
+* **Success Response:**
+
+  * **Code:** 200 <br>
+    **Content:** `{ 'data' : your_data_as_json }`
+ 
+* **Error Response:**
+
+  * **Code:** 404 NOT FOUND <br>
+    **Content:** `{ 'error' : 'Not found' }`
+
+  OR
+
+  * **Code:** 400 BAD REQUEST <br>
+    **Content:** `{ 'error' : 'Bad request' }`
+
+* **Sample Call:**
+
+  ```javascript
+    $.ajax({
+      url: "/d41d8cd98f00b204e9800998ecf8427e",
+      type : "GET",
+      success : function(r) {
+        console.log(r);
+      }
+    });
+  ```
+
+### Insert or update data
+
+  Inserts or updates data from the specified user. Returns new data.
+
+* **URL**
+
+  /:user_id
+
+* **Method:**
+
+  `POST` or `PUT`
+  
+*  **URL Params**
+ 
+   `user_id=[32-byte hex-string]`
+
+* **Data Params**
+
+  `your_data_as_json`
+
+* **Success Response:**
+
+  * **Code:** 200 `PUT` || 201 `POST` <br>
+    **Content:** `{ 'data' : your_data_as_json }`
+ 
+* **Error Response:**
+
+  * **Code:** 404 NOT FOUND <br>
+    **Content:** `{ 'error' : 'Not found' }`
+
+  OR
+
+  * **Code:** 400 BAD REQUEST <br>
+    **Content:** `{ 'error' : 'Bad request' }`
+
+  OR
+
+  * **Code:** 413 PAYLOAD TOO LARGE <br>
+    **Content:** `{ 'error' : 'Payload too large' }`
+
+* **Sample Call:**
+
+  ```javascript
+    your_data_as_json = JSON.stringify(your_data)
+    
+    $.ajax({
+      url: "/d41d8cd98f00b204e9800998ecf8427e",
+      type : "POST", // POST || PUT
+      contentType: "application/json",
+      data: your_data_as_json
+      success : function(r) {
+        console.log(r);
+      }
+    });
+  ```
+
+### Delete data
+
+  Delete data from the specified user. Returns `true` if succeeded.
+
+* **URL**
+
+  /:user_id
+
+* **Method:**
+
+  `DELETE`
+  
+*  **URL Params**
+ 
+   `user_id=[32-byte hex-string]`
+
+* **Data Params**
+
+  None
+
+* **Success Response:**
+
+  * **Code:** 200 <br>
+    **Content:** `{ 'result' : true }`
+ 
+* **Error Response:**
+
+  * **Code:** 404 NOT FOUND <br>
+    **Content:** `{ 'error' : 'Not found' }`
+
+  OR
+
+  * **Code:** 400 BAD REQUEST <br>
+    **Content:** `{ 'error' : 'Bad request' }`
+
+* **Sample Call:**
+
+  ```javascript
+    $.ajax({
+      url: "/d41d8cd98f00b204e9800998ecf8427e",
+      type : "DELETE",
+      success : function(r) {
+        console.log(r);
+      }
+    });
+  ```
+
+## Limitations
+
+We believe our users will make a fair use of the platform. Therefore, each user has a really high quota for requests and a 5mb quota for storage.
+
+## License
+
+[MIT License](LICENSE.md)
---
+++ app.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+
+# Libs
+from cfg import cfg
+from flask import Flask, jsonify, abort, request, make_response
+from pymongo import MongoClient, ReturnDocument
+from flask_httpauth import HTTPBasicAuth
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from string import hexdigits
+from hashlib import md5
+from functools import wraps
+from datetime import datetime
+import re
+
+# MongoDB
+client = MongoClient(cfg['mongourl'])
+db = client[cfg['db']]
+collection = db[cfg['collection']]
+
+# App and add-ons
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+limiter = Limiter(app,key_func=get_remote_address,global_limits=["10000 per day", "100 per minute"])
+
+# Errors
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
+@app.errorhandler(401)
+def unauthorized_access(error):
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}),404)
+
+@app.errorhandler(413)
+def payload_too_large(error):
+    return make_response(jsonify({'error': 'Payload too large'}),413)
+
+@app.errorhandler(429)
+def too_many_requests(error):
+    return make_response(jsonify({'error': 'Too many requests'}),429)
+
+# Helpers
+@auth.get_password
+def get_password(username):
+    if username == cfg['user']:
+        return cfg['password']
+    return None
+
+def validate_userid(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        user_id = kwargs['user_id']
+        if not all(c in hexdigits for c in user_id) or not len(user_id) == 32:
+            abort(400)
+        if collection.find_one_and_update({'hash': user_id}, {'$set': {'last-Request': datetime.utcnow()}}) is None:
+            abort(404)
+        return f(*args, **kwargs)
+    return wrapper
+
+def validate_item(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        user_id = kwargs['user_id']
+        item = kwargs['item']
+        data = collection.find_one({'hash': user_id})['data']
+        if item not in data:
+            abort(404)
+        return f(*args, **kwargs)
+    return wrapper
+
+def limit_content_length(max_length, accumulative = False):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            cl = request.content_length
+            if accumulative is True:
+                user_id = kwargs['user_id']
+                item = kwargs['item']
+                data = collection.find_one({'hash': user_id})['data']
+                free_space = max_length - len(data) + len(data[item] if item in data else "")
+            else:
+            	free_space = max_length
+            if cl is not None and cl > free_space:
+                abort(413)
+            return f(*args, **kwargs)
+        return wrapper
+    return decorator
+
+def make_public_data(data, item = 'data'):
+    return {item: data[item]}
+
+def make_public_user(user):
+    new_user = {}
+    for field in user:
+        if field == '_id':
+            new_user[field] = str(user[field])
+        else:
+            new_user[field] = user[field]
+    return new_user
+
+# Admin
+@app.route('/admin/', methods = ['GET'])
+@auth.login_required
+def get_all():
+    cursor = collection.find({})
+    all_data = [make_public_user(user) for user in cursor]
+    return jsonify({'collection': all_data})
+
+@app.route('/admin/', methods = ['DELETE'])
+@auth.login_required
+def delete_all():
+    result = collection.update_many({}, {'$set': {'data': {}}})
+    return jsonify({'result': True, 'deleted': result.modified_count})
+
+@app.route('/admin/destroy', methods = ['DELETE'])
+@auth.login_required
+def reset_all():
+    result = collection.delete_many({})
+    return jsonify({'result': True, 'deleted': result.deleted_count})
+
+# Public
+@app.route('/', methods = ['GET'], defaults={'user_id': '4bd19e518d90d816fb283cf09d6498bf'})
+@app.route('/<user_id>', methods = ['GET'])
+@validate_userid
+def get_data(user_id):
+    user = collection.find_one({'hash': user_id})
+    return jsonify(make_public_data(user))
+
+@app.route('/<user_id>/<item>', methods = ['GET'])
+@validate_userid
+@validate_item
+def get_item(user_id,item):
+    data = collection.find_one({'hash': user_id})['data']
+    return jsonify(make_public_data(data,item))
+
+@app.route('/', methods = ['POST', 'PUT'], defaults={'user_id': '4bd19e518d90d816fb283cf09d6498bf'})
+@app.route('/<user_id>', methods = ['POST', 'PUT'])
+@validate_userid
+@limit_content_length(5 * 1024 * 1024)
+def update_data(user_id):
+    user = collection.find_one_and_update({'hash': user_id}, {'$set': {'data': request.json}}, return_document = ReturnDocument.AFTER)
+    status = 201 if request.method == "POST" else 200
+    return jsonify(make_public_data(user)), status
+
+@app.route('/<user_id>/<item>', methods = ['POST', 'PUT'])
+@validate_userid
+@limit_content_length(5 * 1024 * 1024, True)
+def update_item(user_id,item):
+    data = collection.find_one_and_update({'hash': user_id}, {'$set': {'data.' + item: request.json}}, return_document = ReturnDocument.AFTER)['data']
+    status = 201 if request.method == "POST" else 200
+    return jsonify(make_public_data(data,item)), status
+
+@app.route('/', methods = ['DELETE'], defaults={'user_id': '4bd19e518d90d816fb283cf09d6498bf'})
+@app.route('/<user_id>', methods = ['DELETE'])
+@validate_userid
+def delete_data(user_id):
+    collection.find_one_and_update({'hash': user_id}, {'$set': {'data': {}}})
+    return jsonify({'result': True})
+
+@app.route('/<user_id>/<item>', methods = ['DELETE'])
+@validate_userid
+@validate_item
+def delete_item(user_id,item):
+    collection.find_one_and_update({'hash': user_id}, {'$unset': {'data.' + item: ""}})
+    return jsonify({'result': True})
+
+# Signup
+@app.route('/signup', methods = ['POST'])
+@limit_content_length(1024)
+def create_user():
+    user = {}
+    user['email'] = request.form['email']
+    if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", user['email']):
+        abort(400)
+    hash = md5(user['email'].encode('utf-8')).hexdigest()
+    user['hash'] = hash
+    if not collection.find_one({'hash': hash}):
+        user['data'] = {}
+        user['created-At'] = datetime.utcnow()
+        collection.insert_one(user)
+    return jsonify({'hash': hash}), 201, {'Access-Control-Allow-Origin': '*'} 
+
+# Run
+if __name__ == '__main__':
+    app.run(debug = True)

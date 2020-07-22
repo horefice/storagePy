e6bf3eb56e26f884ede7dedc9c4b84eaba553348
Henrique Rodrigues Orefice <h.orefice@gmail.com>
2020-07-22 15:27:21 -0300

    added CI and codestyle black

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![Build Status](https://travis-ci.org/horefice/storagePy.svg?branch=master)](https://travis-ci.org/horefice/storagePy)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
 [![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE.md)
 
 # storagePy
--- app.py
+++ app.py
@@ -21,7 +21,9 @@ load_dotenv(os.path.join(basedir, ".env"))
 # App and add-ons
 app = Flask(__name__)
 auth = HTTPBasicAuth()
-limiter = Limiter(app,key_func=get_remote_address,global_limits=["10000 per day", "100 per minute"])
+limiter = Limiter(
+    app, key_func=get_remote_address, global_limits=["10000 per day", "100 per minute"]
+)
 
 # MongoDB
 client = MongoClient(os.environ.get("DATABASE_URL"))
@@ -31,23 +33,28 @@ collection = db[os.environ.get("DATABASE_COLLECTION")]
 # Errors
 @app.errorhandler(400)
 def bad_request(error):
-    return make_response(jsonify({'error': 'Bad request'}), 400)
+    return make_response(jsonify({"error": "Bad request"}), 400)
+
 
 @app.errorhandler(401)
 def unauthorized_access(error):
-    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+    return make_response(jsonify({"error": "Unauthorized access"}), 401)
+
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}),404)
+    return make_response(jsonify({"error": "Not found"}), 404)
+
 
 @app.errorhandler(413)
 def payload_too_large(error):
-    return make_response(jsonify({'error': 'Payload too large'}),413)
+    return make_response(jsonify({"error": "Payload too large"}), 413)
+
 
 @app.errorhandler(429)
 def too_many_requests(error):
-    return make_response(jsonify({'error': 'Too many requests'}),429)
+    return make_response(jsonify({"error": "Too many requests"}), 429)
+
 
 # Helpers
 @auth.get_password
@@ -56,140 +63,185 @@ def get_password(username):
         return os.environ.get("DATABASE_PASSWORD")
     return None
 
+
 def validate_userid(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
-        user_id = kwargs['user_id']
+        user_id = kwargs["user_id"]
         if not all(c in hexdigits for c in user_id) or not len(user_id) == 32:
             abort(400)
-        if collection.find_one_and_update({'hash': user_id}, {'$set': {'last-Request': datetime.utcnow()}}) is None:
+        if (
+            collection.find_one_and_update(
+                {"hash": user_id}, {"$set": {"last-Request": datetime.utcnow()}}
+            )
+            is None
+        ):
             abort(404)
         return f(*args, **kwargs)
+
     return wrapper
+
 
 def validate_item(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
-        user_id = kwargs['user_id']
-        item = kwargs['item']
-        data = collection.find_one({'hash': user_id})['data']
+        user_id = kwargs["user_id"]
+        item = kwargs["item"]
+        data = collection.find_one({"hash": user_id})["data"]
         if item not in data:
             abort(404)
         return f(*args, **kwargs)
+
     return wrapper
 
-def limit_content_length(max_length, accumulative = False):
+
+def limit_content_length(max_length, accumulative=False):
     def decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
             cl = request.content_length
             if accumulative is True:
-                user_id = kwargs['user_id']
-                item = kwargs['item']
-                data = collection.find_one({'hash': user_id})['data']
-                free_space = max_length - len(data) + len(data[item] if item in data else "")
+                user_id = kwargs["user_id"]
+                item = kwargs["item"]
+                data = collection.find_one({"hash": user_id})["data"]
+                free_space = (
+                    max_length - len(data) + len(data[item] if item in data else "")
+                )
             else:
-            	free_space = max_length
+                free_space = max_length
             if cl is not None and cl > free_space:
                 abort(413)
             return f(*args, **kwargs)
+
         return wrapper
+
     return decorator
 
-def make_public_data(data, item = 'data'):
+
+def make_public_data(data, item="data"):
     return {item: data[item]}
+
 
 def make_public_user(user):
     new_user = {}
     for field in user:
-        if field == '_id':
+        if field == "_id":
             new_user[field] = str(user[field])
         else:
             new_user[field] = user[field]
     return new_user
 
+
 # Admin
-@app.route('/admin/', methods = ['GET'])
+@app.route("/admin/", methods=["GET"])
 @auth.login_required
 def get_all():
     cursor = collection.find({})
     all_data = [make_public_user(user) for user in cursor]
-    return jsonify({'collection': all_data})
+    return jsonify({"collection": all_data})
 
-@app.route('/admin/', methods = ['DELETE'])
+
+@app.route("/admin/", methods=["DELETE"])
 @auth.login_required
 def delete_all():
-    result = collection.update_many({}, {'$set': {'data': {}}})
-    return jsonify({'result': True, 'deleted': result.modified_count})
+    result = collection.update_many({}, {"$set": {"data": {}}})
+    return jsonify({"result": True, "deleted": result.modified_count})
 
-@app.route('/admin/destroy', methods = ['DELETE'])
+
+@app.route("/admin/destroy", methods=["DELETE"])
 @auth.login_required
 def reset_all():
     result = collection.delete_many({})
-    return jsonify({'result': True, 'deleted': result.deleted_count})
+    return jsonify({"result": True, "deleted": result.deleted_count})
+
 
 # Public
-@app.route('/', methods = ['GET'], defaults={'user_id': '4bd19e518d90d816fb283cf09d6498bf'})
-@app.route('/<user_id>', methods = ['GET'])
+@app.route(
+    "/", methods=["GET"], defaults={"user_id": "4bd19e518d90d816fb283cf09d6498bf"}
+)
+@app.route("/<user_id>", methods=["GET"])
 @validate_userid
 def get_data(user_id):
-    user = collection.find_one({'hash': user_id})
+    user = collection.find_one({"hash": user_id})
     return jsonify(make_public_data(user))
 
-@app.route('/<user_id>/<item>', methods = ['GET'])
+
+@app.route("/<user_id>/<item>", methods=["GET"])
 @validate_userid
 @validate_item
-def get_item(user_id,item):
-    data = collection.find_one({'hash': user_id})['data']
-    return jsonify(make_public_data(data,item))
+def get_item(user_id, item):
+    data = collection.find_one({"hash": user_id})["data"]
+    return jsonify(make_public_data(data, item))
 
-@app.route('/', methods = ['POST', 'PUT'], defaults={'user_id': '4bd19e518d90d816fb283cf09d6498bf'})
-@app.route('/<user_id>', methods = ['POST', 'PUT'])
+
+@app.route(
+    "/",
+    methods=["POST", "PUT"],
+    defaults={"user_id": "4bd19e518d90d816fb283cf09d6498bf"},
+)
+@app.route("/<user_id>", methods=["POST", "PUT"])
 @validate_userid
 @limit_content_length(5 * 1024 * 1024)
 def update_data(user_id):
-    user = collection.find_one_and_update({'hash': user_id}, {'$set': {'data': request.json}}, return_document = ReturnDocument.AFTER)
+    user = collection.find_one_and_update(
+        {"hash": user_id},
+        {"$set": {"data": request.json}},
+        return_document=ReturnDocument.AFTER,
+    )
     status = 201 if request.method == "POST" else 200
     return jsonify(make_public_data(user)), status
 
-@app.route('/<user_id>/<item>', methods = ['POST', 'PUT'])
+
+@app.route("/<user_id>/<item>", methods=["POST", "PUT"])
 @validate_userid
 @limit_content_length(5 * 1024 * 1024, True)
-def update_item(user_id,item):
-    data = collection.find_one_and_update({'hash': user_id}, {'$set': {'data.' + item: request.json}}, return_document = ReturnDocument.AFTER)['data']
+def update_item(user_id, item):
+    data = collection.find_one_and_update(
+        {"hash": user_id},
+        {"$set": {"data." + item: request.json}},
+        return_document=ReturnDocument.AFTER,
+    )["data"]
     status = 201 if request.method == "POST" else 200
-    return jsonify(make_public_data(data,item)), status
+    return jsonify(make_public_data(data, item)), status
 
-@app.route('/', methods = ['DELETE'], defaults={'user_id': '4bd19e518d90d816fb283cf09d6498bf'})
-@app.route('/<user_id>', methods = ['DELETE'])
+
+@app.route(
+    "/", methods=["DELETE"], defaults={"user_id": "4bd19e518d90d816fb283cf09d6498bf"}
+)
+@app.route("/<user_id>", methods=["DELETE"])
 @validate_userid
 def delete_data(user_id):
-    collection.find_one_and_update({'hash': user_id}, {'$set': {'data': {}}})
-    return jsonify({'result': True})
+    collection.find_one_and_update({"hash": user_id}, {"$set": {"data": {}}})
+    return jsonify({"result": True})
 
-@app.route('/<user_id>/<item>', methods = ['DELETE'])
+
+@app.route("/<user_id>/<item>", methods=["DELETE"])
 @validate_userid
 @validate_item
-def delete_item(user_id,item):
-    collection.find_one_and_update({'hash': user_id}, {'$unset': {'data.' + item: ""}})
-    return jsonify({'result': True})
+def delete_item(user_id, item):
+    collection.find_one_and_update({"hash": user_id}, {"$unset": {"data." + item: ""}})
+    return jsonify({"result": True})
+
 
 # Signup
-@app.route('/signup', methods = ['POST'])
+@app.route("/signup", methods=["POST"])
 @limit_content_length(1024)
 def create_user():
     user = {}
-    user['email'] = request.form['email']
-    if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", user['email']):
+    user["email"] = request.form["email"]
+    if not re.match(
+        r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", user["email"]
+    ):
         abort(400)
-    hash = md5(user['email'].encode('utf-8')).hexdigest()
-    user['hash'] = hash
-    if not collection.find_one({'hash': hash}):
-        user['data'] = {}
-        user['created-At'] = datetime.utcnow()
+    hash = md5(user["email"].encode("utf-8")).hexdigest()
+    user["hash"] = hash
+    if not collection.find_one({"hash": hash}):
+        user["data"] = {}
+        user["created-At"] = datetime.utcnow()
         collection.insert_one(user)
-    return jsonify({'hash': hash}), 201, {'Access-Control-Allow-Origin': '*'} 
+    return jsonify({"hash": hash}), 201, {"Access-Control-Allow-Origin": "*"}
+
 
 # Run
-if __name__ == '__main__':
-    app.run(debug = True)
+if __name__ == "__main__":
+    app.run(debug=True)
